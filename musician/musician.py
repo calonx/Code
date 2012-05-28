6f819cfeb29e518efd41788bedf95bd7c047e370
Paul Marden <pmarden@gmail.com>
2012-05-28 11:51:49 -0700

    Added basic randomized queue. Need to figure out why only MP3 files work...

--- musician/musician.py
+++ musician/musician.py
@@ -1,47 +1,101 @@
 import sys
 import os
+import random
 from PyQt4 import QtGui, QtCore
 from PyQt4.phonon import Phonon
 from playerControl import Ui_PlayerControl
 
 testFile = r"C:\Files\Dropbox\Music\Classical\Pachelbel, Johann\Canon In D.mp3"
 
+class MediaEntry:
+	def __init__ (self, path):
+		self.path = path
+
 class MediaLibrary:
 	def __init__ (self):
-		self.files = []
+		self.entries = []
 
 	def loadFromDirectory (self, directory):
 		for path, dirs, files in os.walk(directory):
 			for filePath in [os.path.join(path, f) for f in files]:
-				files.append(filePath)
+				self.entries.append(MediaEntry(filePath))
+
+class MediaQueue:
+	def __init__ (self, library):
+		self.library = library
+		self.nextQueue = []
+		self.prevQueue = []
+		self.current = None
+
+	def moveNext (self):
+		# Add current track to the history
+		if self.current:
+			self.prevQueue.append(self.current)
+
+		# If there's a track queued, use that; otherwise choose a random one
+		if len(self.nextQueue) > 0:
+			self.current = self.nextQueue.pop(0)
+		else:
+			self.current = random.choice(self.library.entries)
+		return self.current
+
+	def movePrev (self):
+		if self.current:
+			self.nextQueue.insert(0, self.current)
+		if len(self.prevQueue) > 0:
+			self.current = self.prevQueue.pop()
+		else:
+			self.current = None
+		return self.current
+
 
 class MediaPlayer:
 	def __init__ (self):
-		self.library = MediaLibrary()
-		self.library.loadFromDirectory(r'C:\files\dropbox\music')
 		self.audio = Phonon.createPlayer(Phonon.MusicCategory)
 		self.totalTime = 0
-		self.audioSource = Phonon.MediaSource(testFile)
-		self.audio.setCurrentSource(self.audioSource)
+		self.audioSource = None
 		self.audio.totalTimeChanged.connect(self.onTotalTimeChanged)
+		self.nowPlaying = None
+
+		self.library = MediaLibrary()
+		self.library.loadFromDirectory(r'C:\files\dropbox\music')
+		self.queue = MediaQueue(self.library)
 
 	def onTotalTimeChanged (self, time):
 		self.totalTime = time
 
 	def play (self):
+		if not self.nowPlaying:
+			return
 		if self.audio.state() == Phonon.PlayingState:
+			print("Paused: " + self.nowPlaying)
 			self.audio.pause()
 		else:
+			print("Playing: " + self.nowPlaying)
 			self.audio.play()
 
 	def stop (self):
+		if self.nowPlaying:
+			print("Stopped: " + self.nowPlaying)
 		self.audio.stop()
 
 	def prev (self):
-		self.stop()
+		self.setTrack(self.queue.movePrev().path)
 
 	def next (self):
-		self.stop()
+		self.setTrack(self.queue.moveNext().path)
+
+	def setTrack (self, track):
+		wasPlaying = False
+		if self.audio.state() != Phonon.StoppedState:
+			self.stop()
+			wasPlaying = True
+
+		self.nowPlaying = track
+		self.audioSource = Phonon.MediaSource(self.nowPlaying)
+		self.audio.setCurrentSource(self.audioSource)
+		if wasPlaying:
+			self.play()
 
 	def getTimeRemaining (self):
 		return self.audio.remainingTime() / 1000.0
@@ -53,10 +107,15 @@ class MediaPlayer:
 		return self.audio.state() == Phonon.StoppedState
 
 	def getTitle (self):
+		if not self.nowPlaying:
+			return "[no song playing]"
+
 		title = self.audio.metaData("TITLE")
 		if len(title) > 0:
 			return title[0]
-		return "[not available]"
+		
+		title = os.path.split(self.nowPlaying)[1]
+		return title
 
 	def seek (self, time):
 		self.audio.seek(time * 1000)
@@ -91,7 +150,7 @@ class MainForm (QtGui.QMainWindow):
 	def onTimer (self):
 		if self.ui.sliderTime.isSliderDown():
 			return
-			
+
 		self.ui.editTitle.setPlainText(self.player.getTitle())
 
 		pos = 0
